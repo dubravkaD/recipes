b08ca37a7a4fb7d05abb71603a7acf1a10e44bd5
Dubravka Drčelić <duca.drcelic25@gmail.com>
2024-08-20 18:20:47 +0200

    modules modified,global,variables modified

--- src/app/tabs/profile/profile.module.ts
+++ src/app/tabs/profile/profile.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import {RecipeModalComponent} from "./recipe-modal/recipe-modal.component";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    ReactiveFormsModule
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage,RecipeModalComponent]
 })
 export class ProfilePageModule {}
