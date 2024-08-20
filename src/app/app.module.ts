6596efec3451a0ea933a9bf3b296de69777af2f5
Dubravka Drčelić <duca.drcelic25@gmail.com>
2024-08-20 17:49:31 +0200

    component added to modules ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {RecipeModalComponent} from "./tabs/profile/recipe-modal/recipe-modal.component";
+import {RecipeElementComponent} from "./tabs/recipe-element/recipe-element.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RecipeModalComponent, RecipeElementComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
